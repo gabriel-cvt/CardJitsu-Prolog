cb554cd021218b350e15b82bd5cd9850cf182055
David Joshua <128199608+daviidjoshua@users.noreply.github.com>
2024-10-06 23:52:35 -0300

    Atualização em Baralho!

--- src/Types/baralho.pl
+++ src/Types/baralho.pl
@@ -1,7 +1,14 @@
-:- consult('carta.pl').
+:- module(baralho, [novo_baralho/1,
+                    pegar_carta/3,
+                    extrair_cartas/4,
+                    embaralhar/3]).
+                    
+:- use_module('./src/Types/carta.pl').
 
 baralho([Carta]) :- carta(Carta).
 
+% Regra que gera todas as possíveis cartas de um elemento
+% Recebe um Elemento e cria todas as cartas necessárias.
 gerar_cartas_elemento(Elemento, Cartas) :-
     gerar_cartas_elemento(Elemento, 1, Cartas).
 
@@ -18,6 +25,9 @@ gerar_cartas_elemento(Elemento, Valor, [Carta | Cartas]) :-
     ProxValor is Valor + 1,
     gerar_cartas_elemento(Elemento, ProxValor, Cartas).
 
+% Regra responsável por criar um novo baralho,
+% gera cartas de todos os elementos, concatena
+% e as retorna embaralhadas.
 novo_baralho(Baralho) :-
     gerar_cartas_elemento(fogo, CartasFogo),
     gerar_cartas_elemento(agua, CartasAgua),
@@ -26,9 +36,22 @@ novo_baralho(Baralho) :-
     append(FogoAgua, CartasNeve, BaralhoTemp),
     embaralhar(BaralhoTemp, Baralho).
 
+% Regra que pega uma carta do baralho
+% retorna a primeira carta e o baralho atualizado.
 pegar_carta([], none, []).
-pegar_carta([Carta|Cartas], Carta).
+pegar_carta([Carta|Cartas], Carta, Cartas).
 
+% Regra que extrai n cartas do baralho
+% retorna uma lista de cartas que são acumuladas
+% na recursão e também retorna o resto do baralho.
+extrair_cartas(0, Baralho, [], Baralho).
+extrair_cartas(Qtd, Baralho, [Carta|Cartas], Resto):-
+    Qtd > 0,
+    pegar_carta(Baralho, Carta, RestoBaralho),
+    NextQtd is Qtd - 1,
+    extrair_cartas(NextQtd, RestoBaralho, Cartas, Resto).
+
+% Regra que embaralha uma lista.
 embaralhar(Lista, ListaEmbaralhada) :-
     length(Lista, N),
     gerar_indices(N, Indices),
